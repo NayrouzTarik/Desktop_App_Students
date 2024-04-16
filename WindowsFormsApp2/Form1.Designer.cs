cff376815f75d1d38e45e919a7b6115c1f5fef06
Nayrouz <Nayrouz@LAPTOP-1EO91IJ6>
2024-04-16 01:28:07 +0100

    Ajoutez des fichiers projet.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtNom;
+        private System.Windows.Forms.TextBox txtPrenom;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtGenre;
+        private System.Windows.Forms.TextBox txtBranche;
+        private System.Windows.Forms.Button btnAddData;
+        private System.Windows.Forms.Button btnCheckConnection;
+        private System.Windows.Forms.Button btnDeleteData;
+        private System.Windows.Forms.Button btnUpdateData;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnCheckConnection = new System.Windows.Forms.Button();
+            this.btnAddData = new System.Windows.Forms.Button();
+            this.btnDeleteData = new System.Windows.Forms.Button();
+            this.btnUpdateData = new System.Windows.Forms.Button();
+            this.txtNom = new System.Windows.Forms.TextBox();
+            this.txtPrenom = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtGenre = new System.Windows.Forms.TextBox();
+            this.txtBranche = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 107);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.Size = new System.Drawing.Size(971, 460);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnCheckConnection
+            // 
+            this.btnCheckConnection.BackColor = System.Drawing.Color.LightBlue;
+            this.btnCheckConnection.Location = new System.Drawing.Point(12, 12);
+            this.btnCheckConnection.Name = "btnCheckConnection";
+            this.btnCheckConnection.Size = new System.Drawing.Size(150, 40);
+            this.btnCheckConnection.TabIndex = 1;
+            this.btnCheckConnection.Text = "Check Connection";
+            this.btnCheckConnection.Click += new System.EventHandler(this.btn_check_cnt);
+            // 
+            // btnAddData
+            // 
+            this.btnAddData.BackColor = System.Drawing.Color.LightGreen;
+            this.btnAddData.Location = new System.Drawing.Point(170, 12);
+            this.btnAddData.Name = "btnAddData";
+            this.btnAddData.Size = new System.Drawing.Size(150, 40);
+            this.btnAddData.TabIndex = 2;
+            this.btnAddData.Text = "Add Data";
+            this.btnAddData.Click += new System.EventHandler(this.btnAddData_Click);
+            // 
+            // btnDeleteData
+            // 
+            this.btnDeleteData.BackColor = System.Drawing.Color.Salmon;
+            this.btnDeleteData.Location = new System.Drawing.Point(330, 12);
+            this.btnDeleteData.Name = "btnDeleteData";
+            this.btnDeleteData.Size = new System.Drawing.Size(150, 40);
+            this.btnDeleteData.TabIndex = 3;
+            this.btnDeleteData.Text = "Delete Data";
+            this.btnDeleteData.Click += new System.EventHandler(this.btnDeleteData_Click);
+            // 
+            // btnUpdateData
+            // 
+            this.btnUpdateData.BackColor = System.Drawing.Color.Yellow;
+            this.btnUpdateData.Location = new System.Drawing.Point(490, 12);
+            this.btnUpdateData.Name = "btnUpdateData";
+            this.btnUpdateData.Size = new System.Drawing.Size(190, 40);
+            this.btnUpdateData.TabIndex = 3;
+            this.btnUpdateData.Text = "Update Data";
+            this.btnUpdateData.Click += new System.EventHandler(this.btnUpdateData_Click);
+            // 
+            // txtNom
+            // 
+            this.txtNom.Location = new System.Drawing.Point(12, 60);
+            this.txtNom.Name = "txtNom";
+            this.txtNom.Size = new System.Drawing.Size(100, 26);
+            this.txtNom.TabIndex = 3;
+            // 
+            // txtPrenom
+            // 
+            this.txtPrenom.Location = new System.Drawing.Point(118, 60);
+            this.txtPrenom.Name = "txtPrenom";
+            this.txtPrenom.Size = new System.Drawing.Size(100, 26);
+            this.txtPrenom.TabIndex = 4;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(224, 60);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(100, 26);
+            this.txtAge.TabIndex = 5;
+            // 
+            // txtGenre
+            // 
+            this.txtGenre.Location = new System.Drawing.Point(330, 60);
+            this.txtGenre.Name = "txtGenre";
+            this.txtGenre.Size = new System.Drawing.Size(100, 26);
+            this.txtGenre.TabIndex = 6;
+            // 
+            // txtBranche
+            // 
+            this.txtBranche.Location = new System.Drawing.Point(436, 60);
+            this.txtBranche.Name = "txtBranche";
+            this.txtBranche.Size = new System.Drawing.Size(100, 26);
+            this.txtBranche.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(995, 600);
+            this.Controls.Add(this.txtBranche);
+            this.Controls.Add(this.txtGenre);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtPrenom);
+            this.Controls.Add(this.txtNom);
+            this.Controls.Add(this.btnAddData);
+            this.Controls.Add(this.btnDeleteData);
+            this.Controls.Add(this.btnUpdateData);
+            this.Controls.Add(this.btnCheckConnection);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Data Management System";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        NpgsqlConnection conn;
+
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new NpgsqlConnection("host=localhost;port=5432;Database=postgres;Username=postgres;Password=1234");
+        }
+
+        // Check Connection Button Click Event
+        private void btn_check_cnt(object sender, EventArgs e)
+        {
+            conn.Open();
+            MessageBox.Show(conn.State.ToString());
+            conn.Close();
+            MessageBox.Show(conn.State.ToString());
+        }
+
+        // Form Load Event
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        // Affichage du data from data base
+        private void LoadData()
+        {
+            string query = "SELECT * FROM public.\"TB1\"";
+            using (NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(query, conn))
+            {
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+                dataGridView1.DataSource = dataTable;
+            }
+        }
+
+        // Ajout Data Button Click Event
+        private void btnAddData_Click(object sender, EventArgs e)
+        {
+            string nom = txtNom.Text;
+            string prenom = txtPrenom.Text;
+            string age = txtAge.Text;
+            string gender = txtGenre.Text;
+            string branche = txtBranche.Text;
+
+            string query = "INSERT INTO public.\"TB1\" (nom, prenom, age, gender, branche) VALUES (@Nom, @Prenom, @Age, @Gender, @Branche)";
+
+            using (NpgsqlCommand cmd = new NpgsqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@Nom", nom);
+                cmd.Parameters.AddWithValue("@Prenom", prenom);
+                cmd.Parameters.AddWithValue("@Age", age);
+                cmd.Parameters.AddWithValue("@Gender", gender);
+                cmd.Parameters.AddWithValue("@Branche", branche);
+
+                try
+                {
+                    conn.Open();
+                    int rowsAffected = cmd.ExecuteNonQuery();
+                    MessageBox.Show($"{rowsAffected} row(s) inserted successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    LoadData();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error inserting data: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        // Supprision Data Button Click Event
+        private void btnDeleteData_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int id = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["id_etu"].Value);
+
+                string query = "DELETE FROM public.\"TB1\" WHERE ID_ETU = @ID";
+                using (NpgsqlCommand cmd = new NpgsqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@ID", id);
+
+                    try
+                    {
+                        conn.Open();
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        MessageBox.Show($"{rowsAffected} row(s) deleted successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        LoadData();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Error deleting data: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    finally
+                    {
+                        conn.Close();
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a row to delete.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        private void btnUpdateData_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int id = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["id_etu"].Value);
+
+                string nom = txtNom.Text;
+                string prenom = txtPrenom.Text;
+                string age = txtAge.Text;
+                string genre = txtGenre.Text;
+                string branche = txtBranche.Text;
+
+                string query = "UPDATE public.\"TB1\" SET nom = @Nom, prenom = @Prenom, age = @Age, gender = @Genre, branche = @Branche WHERE ID_ETU = @ID";
+
+                using (NpgsqlCommand cmd = new NpgsqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@Nom", nom);
+                    cmd.Parameters.AddWithValue("@Prenom", prenom);
+                    cmd.Parameters.AddWithValue("@Age", age);
+                    cmd.Parameters.AddWithValue("@Genre", genre);
+                    cmd.Parameters.AddWithValue("@Branche", branche);
+                    cmd.Parameters.AddWithValue("@ID", id);
+
+                    try
+                    {
+                        conn.Open();
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        MessageBox.Show($"{rowsAffected} row(s) updated successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        LoadData();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Error updating data: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    finally
+                    {
+                        conn.Close();
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a row to update.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+    }
+}
